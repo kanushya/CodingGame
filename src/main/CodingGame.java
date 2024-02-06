6ec75a8135b8130dabf6a369d775ace25420b5b9
kanushya <kanushyav@gmail.com>
2024-02-06 21:14:15 +0530

    removed

--- src/main/CodingGame.java
+++ src/main/CodingGame.java
@@ -7,6 +7,5 @@ import java.util.ArrayList;
 public class CodingGame {
     public static void main(String[] args) {
         List<User> users= new ArrayList<>();
-    }
-    // public
+    }git add
 }
